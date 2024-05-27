ad3e0467a980ee8b0805515fe54364c671e5a514
Reed Jackson <reed.a.jackson@gmail.com>
2024-05-27 17:18:04 -0400

    x

---
+++ supabase.js
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey, {
+	auth: {
+		autoRefreshToken: false,
+		persistSession: false,
+		detectSessionInUrl: false,
+	},
+});
+
+export default supabase;
